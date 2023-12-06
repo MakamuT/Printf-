98c7994e57fe184239dddd4bd92b5ef48c28eb89
Adora <adoraonyile@gmail.com>
2023-12-06 14:56:13 +0100

    bug fix

--- print_int.c
+++ print_int.c
@@ -22,13 +22,13 @@ int print_int(va_list arg)
 		len += _putchar(45);
 		num = i * -1;
 	}
-	if (i > 0)
-		num = i;
-	if (num == 0)
+	if (i == 0)
 	{
 		len += _putchar(48);
 		return (1);
 	}
+	if (i > 0)
+		num = i;
 	for (; num / div > 9;)
 		div *= 10;
 	for (; div != 0;)
